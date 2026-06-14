799fc8cab295c0c67768a64c6759b282e91c09b2
salazar20101804 <ykfj7v5npf@privaterelay.appleid.com>
2026-06-14 12:25:21 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+cards.forEach(card => {
+
+observer.observe(card);
+
+});
